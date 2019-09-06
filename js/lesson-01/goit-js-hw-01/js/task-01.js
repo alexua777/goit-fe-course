be3ff295afad962be3320800df0240009dd1be7d
Alex Bakhteyev <a.bakhteyev@gmail.com>
2019-09-06 16:25:53 -0400

    task 01

---
+++ js/lesson-01/goit-js-hw-01/js/task-01.js
@@ -0,0 +1,12 @@
+const name = 'Protection shield generator';
+let price = 1000;
+
+console.log(`Selected "${name}", price per item ${price} credits`);
+
+price = 2000;
+console.log(`Selected "${name}", price per item ${price} credits`);
+
+
+
+
+
