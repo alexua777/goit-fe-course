6b85846e9f5c551c5adf0b5afd9a51ca47eb39d2
Alex Bakhteyev <a.bakhteyev@gmail.com>
2019-11-03 15:58:22 -0500

    updates to task-05.js

--- js/goit-js-hw-07/js/task-05.js
+++ js/goit-js-hw-07/js/task-05.js
@@ -3,6 +3,11 @@ const output = document.querySelector('#name-output');
 input.addEventListener('input', submitHandler);
 
 function submitHandler(event) {
+  console.log(event.currentTarget.value);
+  if (event.currentTarget.value === '') {
+    output.textContent = 'незнакомец';
+    return;
+  }
   output.textContent = event.currentTarget.value;
 
 }
