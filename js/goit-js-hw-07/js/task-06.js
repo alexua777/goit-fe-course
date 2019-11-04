99da8fed99fb05f2d206cd141d79ead2849a1e9a
Alex Bakhteyev <a.bakhteyev@gmail.com>
2019-11-04 12:49:14 -0500

    upodates to task-06

--- js/goit-js-hw-07/js/task-06.js
+++ js/goit-js-hw-07/js/task-06.js
@@ -7,14 +7,18 @@ let resultValue = 0;
 function inputHandler(event) {
   resultValue = event.currentTarget.value.length;
 
+
 }
 
 function resultHandler(event) {
-
+  input.classList.remove('invalid');
+  input.classList.remove('valid');
   if (event.currentTarget === event.target) {
+
     if (resultValue > Number(input.getAttribute('data-length'))) {
       input.classList.add('invalid');
     }
+
     input.classList.add('valid');
   }
 }
