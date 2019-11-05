c0e02ef5ffa48353e5db2017982b825bbda875f6
Alex Bakhteyev <a.bakhteyev@gmail.com>
2019-11-05 14:20:44 -0500

    updates to task-06.js

--- js/goit-js-hw-07/js/task-06.js
+++ js/goit-js-hw-07/js/task-06.js
@@ -1,26 +1,13 @@
 const input = document.querySelector('#validation-input');
 input.addEventListener('input', inputHandler);
-// document.body.addEventListener('click', resultHandler);
 
 let resultValue = 0;
 
 function inputHandler(event) {
-
-
   resultValue = event.currentTarget.value.length;
 
   if (resultValue === Number(input.getAttribute('data-length'))) {
     input.classList.remove('invalid');
     input.classList.add('valid');
-
   } else input.classList.add('invalid');
 }
-
-// function resultHandler(event) {
-//   input.classList.remove('invalid');
-//   input.classList.remove('valid');
-//   if (resultValue === Number(input.getAttribute('data-length'))) {
-//     input.classList.add('valid');
-//   }
-//   input.classList.add('invalid');
-// }
