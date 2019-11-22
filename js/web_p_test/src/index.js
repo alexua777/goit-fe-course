3c8cac9e6197fcf0f9fce659c5feb02aa4d4b7fc
Alex Bakhteyev <a.bakhteyev@gmail.com>
2019-11-22 18:42:09 -0500

    webpack

---
+++ js/web_p_test/src/index.js
@@ -0,0 +1,10 @@
+import message from './message.js';
+import arrow from './arrow';
+import './a.css';
+import './b.css';
+
+console.log(message);
+console.log(moduleName);
+
+console.log(arrow(3, 5));
+
