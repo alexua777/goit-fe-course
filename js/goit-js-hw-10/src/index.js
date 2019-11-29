10d4fc2c1d8bf3174083b5b840e941751e9836e9
Alex Bakhteyev <a.bakhteyev@gmail.com>
2019-11-28 23:16:12 -0500

    added changeTheme function

--- js/goit-js-hw-10/src/index.js
+++ js/goit-js-hw-10/src/index.js
@@ -2,13 +2,17 @@ import './css/styles.css';
 import cards from './utils/menu.json';
 import menuCardItem from './templates/menu-item.hbs';
 
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 const refs = {
   menu: document.querySelector('.js-menu'),
-
   body: document.querySelector('body'),
   switch: document.querySelector('.switch__input'),
 };
+
 buildMenu(cards);
 
 function buildMenu(cards) {
@@ -16,52 +20,38 @@ function buildMenu(cards) {
   console.log(markup);
   refs.menu.insertAdjacentHTML('beforeend', markup);
 }
+
 refs.switch.addEventListener('change', changeTheme);
 
+// Loading Theme from localStorage
 
+let currentTheme = localStorage.getItem('theme');
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
-let currentTheme = Theme.LIGHT;
-function changeTheme() {
-
-  if (currentTheme === Theme.LIGHT) {
-
-    refs.body.classList.add(Theme.DARK);
-    refs.body.classList.remove(Theme.LIGHT);
-    currentTheme = Theme.DARK;
-
-
-  } else {
-
-    refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
-    currentTheme = Theme.LIGHT;
+function loadStoredTheme() {
+  if (currentTheme) {
+    refs.body.classList.add(currentTheme);
   }
 
-
+  if (currentTheme === Theme.DARK) {
+    refs.switch.setAttribute('checked', true);
+  }
 }
 
-//   } else {
-//     refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+loadStoredTheme();
 
-//     currentTheme = Theme.DARK;
-//   }
+// Switching Theme
 
-
-
-
-//   } else {
-//     refs.body.classList.remove(Theme.DARK);
-//     refs.body.classList.add(Theme.LIGHT);
-//     currentTheme = Theme.LIGHT;
-//     console.log(currentTheme);
-//     console.log('dark');
-//   }
-
-
-
-
-
+function changeTheme() {
+  if (currentTheme === Theme.LIGHT || currentTheme === undefined) {
+    refs.body.classList.add(Theme.DARK);
+    refs.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+    currentTheme = Theme.DARK;
+    refs.switch.setAttribute('checked', true);
+  } else {
+    refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+    currentTheme = Theme.LIGHT;
+    refs.switch.setAttribute('checked', false);
+  }
+}
